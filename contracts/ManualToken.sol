ee4cb798c764190b57cc7d0a94acd1ff42bc3201
codersjj <shanewestlife@outlook.com>
2025-05-26 12:54:34 +0800

    Add ManualToken contract implementation
    see: https://eips.ethereum.org/EIPS/eip-20
    see: https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol

---
+++ contracts/ManualToken.sol
@@ -0,0 +1,185 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.28;
+
+error ERC20InvalidSender(address sender);
+
+error ERC20InvalidReceiver(address receiver);
+
+error ERC20InvalidApprover(address approver);
+
+error ERC20InvalidSpender(address spender);
+
+error ERC20InsufficientAllowance(
+    address spender,
+    uint256 allowance,
+    uint256 needed
+);
+
+error ERC20InsufficientBalance(address sender, uint256 balance, uint256 needed);
+
+contract ManualToken {
+    mapping(address account => uint256) private _balances;
+    mapping(address account => mapping(address spender => uint256))
+        private _allowances;
+    uint256 private _totalSupply;
+    string private _name;
+    string private _symbol;
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(
+        address indexed owner,
+        address indexed spender,
+        uint256 value
+    );
+
+    constructor(string memory tokenName_, string memory tokenSymbol_) {
+        _name = tokenName_;
+        _symbol = tokenSymbol_;
+    }
+
+    function name() public view virtual returns (string memory) {
+        return _name;
+    }
+
+    function symbol() public view virtual returns (string memory) {
+        return _symbol;
+    }
+
+    function decimals() public view virtual returns (uint8) {
+        return 18;
+    }
+
+    function totalSupply() public view virtual returns (uint256) {
+        return _totalSupply;
+    }
+
+    function balanceOf(address account) public view virtual returns (uint256) {
+        return _balances[account];
+    }
+
+    function allowance(
+        address owner,
+        address spender
+    ) public view virtual returns (uint256) {
+        return _allowances[owner][spender];
+    }
+
+    // transfer tokens
+    // subtract from address amount and add to to address
+
+    function transfer(
+        address _to,
+        uint256 _value
+    ) public returns (bool success) {
+        address owner = msg.sender;
+        _transfer(owner, _to, _value);
+        return true;
+    }
+
+    function _transfer(address from, address to, uint256 amount) internal {
+        if (from == address(0)) {
+            revert ERC20InvalidSender(address(0));
+        }
+        if (to == address(0)) {
+            revert ERC20InvalidReceiver(address(0));
+        }
+        _update(from, to, amount);
+    }
+
+    function _update(address from, address to, uint256 value) internal {
+        if (from == address(0)) {
+            _totalSupply += value;
+        } else {
+            uint256 fromBalance = _balances[from];
+            if (fromBalance < value) {
+                revert ERC20InsufficientBalance(from, fromBalance, value);
+            }
+            unchecked {
+                _balances[from] = fromBalance - value;
+            }
+        }
+
+        if (to == address(0)) {
+            unchecked {
+                _totalSupply -= value;
+            }
+        } else {
+            unchecked {
+                _balances[to] += value;
+            }
+        }
+
+        emit Transfer(from, to, value);
+    }
+
+    function transferFrom(
+        address from,
+        address to,
+        uint256 amount
+    ) public returns (bool) {
+        address spender = msg.sender;
+        _spendAllowance(from, spender, amount);
+        _transfer(from, to, amount);
+        return true;
+    }
+
+    function _spendAllowance(
+        address owner,
+        address spender,
+        uint256 value
+    ) internal {
+        uint256 currentAllowance = _allowances[owner][spender];
+        if (currentAllowance < value) {
+            revert ERC20InsufficientAllowance(spender, currentAllowance, value);
+        }
+        unchecked {
+            _approve(owner, spender, currentAllowance - value, false);
+        }
+    }
+
+    function approve(
+        address spender,
+        uint256 value
+    ) public virtual returns (bool) {
+        address owner = msg.sender;
+        _approve(owner, spender, value);
+        return true;
+    }
+
+    function _approve(address owner, address spender, uint256 value) internal {
+        _approve(owner, spender, value, true);
+    }
+
+    function _approve(
+        address owner,
+        address spender,
+        uint256 value,
+        bool emitEvent
+    ) internal virtual {
+        if (owner == address(0)) {
+            revert ERC20InvalidApprover(address(0));
+        }
+        if (spender == address(0)) {
+            revert ERC20InvalidSpender(address(0));
+        }
+        _allowances[owner][spender] = value;
+        if (emitEvent) {
+            emit Approval(owner, spender, value);
+        }
+    }
+
+    function _mint(address account, uint256 value) internal {
+        if (account == address(0)) {
+            revert ERC20InvalidReceiver(address(0));
+        }
+        _update(address(0), account, value);
+    }
+
+    function _burn(address account, uint256 value) internal {
+        if (account == address(0)) {
+            revert ERC20InvalidSender(address(0));
+        }
+        _update(account, address(0), value);
+    }
+}
